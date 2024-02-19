825ea101cea4c8f10cde79e504a4f221b67077d8
Zanzicki <144442443+Zanzicki@users.noreply.github.com>
2024-02-19 13:40:25 +0000

    .

--- PathFinder/obj/Debug/net6.0-windows/PathFinder.AssemblyInfo.cs
+++ PathFinder/obj/Debug/net6.0-windows/PathFinder.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PathFinder")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03dc05ce33edc756abb90246ea79770a29476c9f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cff329f1e86561d4a17fb54f2aa5433420a1c198")]
 [assembly: System.Reflection.AssemblyProductAttribute("PathFinder")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PathFinder")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
