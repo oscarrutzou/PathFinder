e4f3824f31450e860ec1abb2a5f8f63af8f81f15
Erik-DK20 <erpr2@hotmail.com>
2024-02-20 10:52:52 +0100

    Cells started

--- PathFinder/Game1.cs
+++ PathFinder/Game1.cs
@@ -1,13 +1,42 @@
-﻿using Microsoft.Xna.Framework;
+﻿using Microsoft.VisualBasic.ApplicationServices;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
+
 
 namespace PathFinder
 {
     public class Game1 : Game
     {
+        private static Game1 instance;
+
+        public static Game1 Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new Game1();
+                }
+                return instance;
+            }
+        }
+
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+
+        public SpriteFont SpriteFont { get; set; }
+
+        private int cellCount = 10;
+
+        private int cellSize = 100;
+
+        public Dictionary<Point, Grid> Cells { get; private set; } = new Dictionary<Point, Grid>();
+
+        public Dictionary<string, Texture2D> sprites { get; private set; } = new Dictionary<string, Texture2D>();
+
+        private Grid start, goal;
 
         public Game1()
         {
@@ -20,6 +49,7 @@ namespace PathFinder
         {
             // TODO: Add your initialization logic here
 
+ 
             base.Initialize();
         }
 
@@ -28,6 +58,27 @@ namespace PathFinder
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
+            SpriteFont = Content.Load<SpriteFont>("TextFont");
+            sprites["Pixel"] = Content.Load<Texture2D>("Pixel");
+         
+
+            for (int y = 0; y < cellCount; y++)
+            {
+                for (int x = 0; x < cellCount; x++)
+                {
+                    Cells.Add(new Point(x, y), new Grid(new Point(x, y), cellSize, cellSize));
+                }
+            }
+
+            foreach (KeyValuePair<Point, Grid> cell in Cells)
+            {
+                cell.Value.LoadContent();
+            }
+
+            _graphics.PreferredBackBufferWidth = cellCount * cellSize + 200;  // set this value to the desired width of your window
+            _graphics.PreferredBackBufferHeight = cellCount * cellSize + 1;   // set this value to the desired height of your window
+            _graphics.ApplyChanges();
+
         }
 
         protected override void Update(GameTime gameTime)
@@ -36,6 +87,10 @@ namespace PathFinder
                 Exit();
 
             // TODO: Add your update logic here
+            foreach (KeyValuePair<Point, Grid> cell in Cells)
+            {
+                cell.Value.Update();
+            }
 
             base.Update(gameTime);
         }
@@ -44,9 +99,20 @@ namespace PathFinder
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
+            _spriteBatch.Begin();
             // TODO: Add your drawing code here
 
+            foreach (KeyValuePair<Point, Grid> cell in Cells)
+            {
+                cell.Value.Draw(_spriteBatch);
+            }
             base.Draw(gameTime);
+            _spriteBatch.End();
+        }
+
+        public void OnCellClick(Grid clicked)
+        {
+            
         }
     }
 }
