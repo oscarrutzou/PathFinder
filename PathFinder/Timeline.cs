ac0d3162fa34f737fcf45df002adb80fe076c8a2
Erik-DK20 <erpr2@hotmail.com>
2024-02-21 10:00:19 +0100

    TimeLine class

---
+++ PathFinder/Timeline.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PathFinder
+{
+    internal class Timeline
+    {
+        private void TimeLine()
+        {
+            //  Choose.Algoritme
+
+            //  SpawnWizard
+
+            //    Wait
+
+            //    Move.Wizard
+            
+            //    Pick.Up.Key
+            
+            //    Wait
+            
+            //    Move.Wizard
+            
+            //    Pick.Up.Potion
+            
+            //    Wait
+            
+            //    Move.Wizard
+            
+            //    Pick.Up.Key
+            
+            //    Wait
+            
+            //    Move.To.Castle
+            
+            //    Wait
+            
+            //    Move.To.Portal
+            
+            //    Wait
+            
+            //    You Win
+            
+            //    Wait
+            
+            //    Restart
+        }
+    }
+}
