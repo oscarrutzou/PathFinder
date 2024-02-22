f839b863bcf62d6d55db7a634491fd1a0c13f6c0
Stefan <58773@student.eadania.dk>
2024-02-22 01:31:42 +0100

    Tilføjet DialogueBox,DialogueBoxManager og en smule logik og fields i Gameworld.

---
+++ PathFinder/Scripts/GameObject/GUI/Dialoguebox.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
+
+namespace PathFinder.Scripts.GameObject.GUI
+{
+    public class DialogueBox
+    {
+        private SpriteFont font;
+        private string text;
+        private Vector2 position;
+        private Texture2D dialogueBoxTexture;
+        private bool isVisible = true;
+        private string currentScenario;
+
+        public DialogueBox(SpriteFont font, string text, Vector2 position)
+        {
+            this.font = font;
+            this.text = text;
+            this.position = position;
+        }
+
+        public bool IsVisible
+        {
+            get { return isVisible; }
+            set { isVisible = value; }
+        }
+
+        public void Update()
+        {
+            KeyboardState keyboardState = Keyboard.GetState();
+            if (keyboardState.IsKeyDown(Keys.Enter))
+            {
+                isVisible = false;
+            }
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            if (isVisible)
+            {
+                spriteBatch.Draw(dialogueBoxTexture, position, Color.White);
+                spriteBatch.DrawString(font, text, GameWorld.Instance.uiCam.BottomCenter, Color.Black);
+            }
+        }
+
+        // Method to update the current scenario
+        public void UpdateScenario(string newScenario)
+        {
+            currentScenario = newScenario;
+        }
+    }
+}
